cf8c7a98032ea59fe5c971fc262e6310e35a0064
Stanislav Král <stanislav.kral3@gmail.com>
2020-12-09 16:34:43 +0100

    almost finished 6th task

--- 06/lib/linReg/normalEqn.m
+++ 06/lib/linReg/normalEqn.m
@@ -1,6 +1,9 @@
-function [theta] =  normalEqn(X, y)
-  theta = zeros(size(X, 2), 1);
-  
-  % no need for feature scaling
-  theta = pinv((X') * X) * X' * y;
+function [theta] =  normalEqn(X, y, options)
+    theta = zeros(size(X, 2), 1);
+    
+    L = options.lambda .* eye(size(X, 2));
+    L(1) = 0;
+
+    % no need for feature scaling
+    theta = pinv((X') * X + L) * X' * y;
 end
